3618941d1c990c6693731f660297554824312718
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-04-07 12:23:46 -0500

    Complete specs for my_any and my_none

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -46,30 +46,76 @@ RSpec.describe 'Enumerable' do
       it 'returns true if all elements in array fits the contition' do
         expect(array_str.my_all? { |i| i =~ /./ }).to eq(true)
       end
+
       it 'returns false if not all elements in array fits the contition' do
         expect(array_str.my_all? { |i| i =~ /^[a-zA-Z]*$/ }).to eq(false)
       end
     end
+
     context 'there is no block given' do
       it 'will return true if all the items are not nil nor false' do
         expect(numbers.my_all?).to be_truthy
       end
-      it 'will return false if at least one item is nil or false' do
-        expect(numbers_nil.my_all?).to be_falsy
-      end
+
       it 'will return true if all the elements are of a given class' do
         expect(numbers.my_all?(Integer)).to eq(true)
       end
-      it 'will return false if not all the elements are of a given class' do
-        expect(array_str.my_all?(Numeric)).to eq(false)
-      end
+
       it 'will return true if all items match regular expression' do
         expect(numbers.my_all?(/\d/)).to eq(true)
-      end
-      it 'will return false if not all items match regular expression' do
-        expect(numbers_nil.my_all?(/\d/)).to eq(false)
       end
     end
   end
 
+  describe '.my_any' do
+    context 'there is a block given' do
+      it 'returns true if any element in array fits the contition' do
+        expect(array_str.my_any? { |i| i =~ /^[a-zA-Z]*$/ }).to eq(true)
+      end
+
+      it 'returns false if none of the elements fits the condition' do
+        expect(numbers.my_any? { |i| i =~ /^[a-zA-Z]*$/ }).to eq(false)
+      end
+    end
+
+    context 'there is no block given' do
+      it 'will return true if any the items is truthy' do
+        expect(numbers_nil.my_any?).to eql(true)
+      end
+
+      it 'will return true if any element is of a given class' do
+        expect(['s','s',3,'s'].my_any?(Integer)).to eq(true)
+      end
+
+      it 'will return true any item match regular expression' do
+        expect(array_str.my_any?(/\d/)).to eq(true)
+      end
+    end
+  end
+
+  describe '.my_none' do
+    context 'there is a block given' do
+      it 'returns true if none of the elements in array fit the contition' do
+        expect(numbers.my_none? { |i| i =~ /^[a-zA-Z]*$/ }).to eq(true)
+      end
+
+      it 'returns false if any of the elements fit the condition' do
+        expect(array_str.my_none? { |i| i =~ /^[a-zA-Z]*$/ }).to eq(false)
+      end
+    end
+
+    context 'there is no block given' do
+      it 'will return true if none of the items is truthy' do
+        expect([nil, false].my_none?).to eql(true)
+      end
+
+      it 'will return true if any element is of a given class' do
+        expect(['s','s','3','s'].my_none?(Numeric)).to eq(true)
+      end
+
+      it 'will return true if none if the items match regular expression' do
+        expect(numbers.my_none?(/s/)).to eq(true)
+      end
+    end
+  end
 end
